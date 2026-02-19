0d4d79124c56c2a301edc7af1feb05eadcb562e5
Chinmay-Sakhare07 <sakhare.c@northeastern.edu>
2026-02-19 18:36:52 -0500

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'https://medinexus-api-dfefgbbyc5auhncw.eastus2-01.azurewebsites.net';
+const API_BASE_URL = 'https://medinexus-api-dfefgbbyc5auhncw.eastus2-01.azurewebsites.net/api';
 
 const api = axios.create({
   baseURL: API_BASE_URL,
